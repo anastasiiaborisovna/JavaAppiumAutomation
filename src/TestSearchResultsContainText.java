9b25c44ac26631491760ec8f7eb23eea091c0bf2
a.naumova <anastasiiaborisovna@gmail.com>
2019-11-25 11:42:41 +0300

    Ex4*: Тест: проверка слов в поиске Написать тест, который:
    Ищет какое-то слово
    Убеждается, что в каждом результате поиска есть это слово.
    
    Результат выполнения задания нужно отдельным коммитом положить в git. В качестве ответа прислать txt файл со ссылкой на коммит. Если вам потребовалось несколько коммитов для выполнения одного задания - присылайте ссылки на все эти коммиты с комментариями.

---
+++ src/TestSearchResultsContainText.java
@@ -0,0 +1,103 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.List;
+
+import java.net.URL;
+
+public class TestSearchResultsContainText {
+    private AppiumDriver driver;
+
+    @Before
+    public  void setUp() throws Exception {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("deviceName","AndroidTestDevice");
+        capabilities.setCapability("platformVersion","8.0");
+        capabilities.setCapability("automationName","Appium");
+        capabilities.setCapability("appPackage","org.wikipedia");
+        capabilities.setCapability("appActivity",".main.MainActivity");
+        capabilities.setCapability("app","/Users/a.naumova/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown() {
+
+        driver.quit();
+    }
+
+    @Test
+    public void testSearchResultsContainText() {
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search Wikipedia'",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                "Java",
+                "Cannot find search input",
+                5
+        );
+
+        List<WebElement> results = waitForAllElementsPresent(
+                By.id("org.wikipedia:id/page_list_item_title"),
+                "Not found any elements",
+                15);
+
+        String searchLine = "Java";
+        for(WebElement result : results) {
+            String searchResult = result.getAttribute("text");
+            boolean textFound = searchResult.contains(searchLine);
+
+            Assert.assertTrue(
+                    "Cannot find " + searchLine+ " in one of results. The actual result is " + searchResult,
+                    textFound
+            );
+        }
+    }
+
+    private WebElement waitForElementPresent(By by, String errorMessage, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMessage + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+
+    private List<WebElement> waitForAllElementsPresent(By by, String errorMessage, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMessage + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfAllElementsLocatedBy(by)
+        );
+    }
+
+
+    private WebElement waitForElementAndClick(By by, String errorMessage, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementAndSendKeys(By by, String value, String errorMessage, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+
+}
