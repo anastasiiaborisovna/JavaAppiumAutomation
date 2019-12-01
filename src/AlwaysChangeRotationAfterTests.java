453821349021be6d15fd374ea7f2374d3ca5e319
a.naumova <anastasiiaborisovna@gmail.com>
2019-12-01 12:30:22 +0300

    Ex7*: Поворот экрана Appium устроен так, что может сохранить у себя в памяти поворот экрана, который использовался в предыдущем тесте, и начать новый тест с тем же поворотом. Мы написали тест на поворот экрана, и он может сломаться до того, как положение экрана восстановится. Следовательно, если мы запустим несколько тестов одновременно, последующие тесты будут выполняться в неправильном положении экрана, что может привести к незапланированным проблемам.
    Как нам сделать так, чтобы после теста на поворот экрана сам экран всегда оказывался в правильном положении, даже если тест упал в тот момент, когда экран был наклонен?

---
+++ src/AlwaysChangeRotationAfterTests.java
@@ -0,0 +1,118 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.URL;
+
+public class AlwaysChangeRotationAfterTests {
+
+    private AppiumDriver driver;
+
+    @Before
+    public  void setUp() throws Exception {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("deviceName","AndroidTestDevice");
+        capabilities.setCapability("platformVersion","8.0");
+        capabilities.setCapability("automationName","Appium");
+        capabilities.setCapability("appPackage","org.wikipedia");
+        capabilities.setCapability("appActivity",".main.MainActivity");
+        capabilities.setCapability("app","/Users/a.naumova/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+
+    }
+
+    @After
+    public void tearDown() {
+
+        driver.rotate(ScreenOrientation.PORTRAIT);
+        driver.quit();
+    }
+
+
+    @Test
+    public void testChangeScreenOrientationOnSearchResults() {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find Search Wikipedia input",
+                5
+        );
+
+        String searchingLine = "Java";
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                searchingLine,
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id = 'org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                "Cannot find 'Object-oriented programming language' topic searchig by '" +searchingLine+"'",
+                15
+        );
+
+        String titleBeforeRotation = waitForElementAndGettAtribute(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "text",
+                "Cannot find title of article",
+                15
+        );
+
+        driver.rotate(ScreenOrientation.LANDSCAPE);
+
+        String titleAfterRotation = waitForElementAndGettAtribute(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "text",
+                "Cannot find title of article",
+                15
+        );
+
+        Assert.assertEquals(
+                "Article title have been changeen after screen rotation",
+                titleBeforeRotation,
+                titleAfterRotation
+        );
+
+    }
+
+
+    private WebElement waitForElementPresent(By by, String errorMessage, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMessage + "\n");
+
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private WebElement waitForElementAndClick(By by, String errorMessage, long timeoutInSeconds ) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+        element.click();
+        return  element;
+    }
+
+    private WebElement waitForElementAndSendKeys(By by, String value, String errorMessage, long timeoutInSeconds ) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+        element.sendKeys(value);
+        return  element;
+    }
+
+    private String waitForElementAndGettAtribute(By by, String attribute, String errorMessage, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+        return element.getAttribute(attribute);
+    }
+
+
+}
